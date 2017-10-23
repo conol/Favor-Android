29bc1b4db6ea9ebc07cdcf803c4b954a845c8269
itouuuuuuuuu <mfito.13@gmail.com>
2017-10-23 18:51:56 +0900

    Userページのactivity(空)追加

---
+++ app/src/main/java/jp/co/conol/favor_android/activity/UserActivity.java
@@ -0,0 +1,15 @@
+package jp.co.conol.favor_android.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jp.co.conol.favor_android.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
