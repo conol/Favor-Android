ac774ec31e8c22b46ad5101fba8d10831376c720
m.ito <m.ito@conol.jp>
2017-11-05 21:44:37 +0900

    注文完了ページの容易

---
+++ app/src/main/java/jp/co/conol/favor_android/activity/OrderDoneActivity.java
@@ -0,0 +1,15 @@
+package jp.co.conol.favor_android.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jp.co.conol.favor_android.R;
+
+public class OrderDoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_done);
+    }
+}
